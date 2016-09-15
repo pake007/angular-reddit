3fc4ce21d91bba4baf3debdc13694240370c9ccf
Jimmy Huang <pake007@gmail.com>
2016-09-15 14:50:30 +0800

    introduce router

--- app.js
+++ app.js
@@ -3,7 +3,19 @@
 *
 * Description
 */
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider', 
+'$urlRouterProvider', 
+function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+  $urlRouterProvider.otherwise('home');
+}])
 .factory('posts', [function(){
   var o = {
     posts: [
